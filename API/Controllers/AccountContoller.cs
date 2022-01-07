59abdb32eabe3f6583cee99d828c380ddf4dc538
Dylan Thunn <49996239+dthunn@users.noreply.github.com>
2022-01-07 08:31:37 -0700

    initial commit

---
+++ API/Controllers/AccountContoller.cs
@@ -0,0 +1,77 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using API.Data;
+using API.DTOs;
+using API.Entities;
+using API.interfaces;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+	public class AccountController : BaseApiController
+	{
+		private readonly DataContext _context;
+		private readonly ITokenService _tokenService;
+		public IMapper _mapper { get; }
+
+		public AccountController(DataContext context, ITokenService tokenService, IMapper mapper)
+		{
+			_mapper = mapper;
+			_tokenService = tokenService;
+			_context = context;
+		}
+
+		[HttpPost("register")]
+		public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
+		{
+			if (await UserExists(registerDto.Username)) return BadRequest("Username is taken");
+
+			var user = _mapper.Map<AppUser>(registerDto);
+
+			user.UserName = registerDto.Username.ToLower();
+
+			_context.Users.Add(user);
+			await _context.SaveChangesAsync();
+
+			return new UserDto
+			{
+				Username = user.UserName,
+				Token = _tokenService.CreateToken(user),
+				KnownAs = user.KnownAs,
+				Gender = user.Gender
+			};
+		}
+
+
+		[HttpPost("login")]
+		public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
+		{
+			var user = await _context.Users
+			   .Include(p => p.Photos)
+			   .SingleOrDefaultAsync(x => x.UserName == loginDto.Username);
+
+			if (user == null) return Unauthorized("Invalid username");
+
+			return new UserDto
+			{
+				Username = user.UserName,
+				Token = _tokenService.CreateToken(user),
+				PhotoUrl = user.Photos.FirstOrDefault(x => x.IsMain)?.Url,
+				KnownAs = user.KnownAs,
+				Gender = user.Gender
+
+			};
+		}
+
+		private async Task<bool> UserExists(string username)
+		{
+			return await _context.Users.AnyAsync(x => x.UserName == username.ToLower());
+		}
+	}
+}
